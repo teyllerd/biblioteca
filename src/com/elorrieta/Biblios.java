c232e49cf6d8426e972d20c9a18d3f9994098702
Egoitz <infegallaga@elorrieta-errekamari.com>
2019-12-05 16:18:31 +0100

    Inicio

---
+++ src/com/elorrieta/Biblios.java
@@ -0,0 +1,10 @@
+package com.elorrieta;
+
+public class Biblios {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Inicio de proyecto.");
+	}
+
+}
